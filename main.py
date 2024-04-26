d01aec77af52fb21319e7e7f3ddd976cf5007690
Magical-Pubah <bconnellypitts@gmail.com>
2024-04-26 02:10:51 -0700

    Added sort by decending values for letter counts and referenced other examples for syntax help, possible to reduce code further

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     story = get_book_test(book_path)
+    lett_count = letter_count(story)
+    organized_list = organize(lett_count)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"There are {word_count(story)} words in this book.")
-    print(letter_count(story))
+    print()
+    for letter in organized_list:
+        print(f"The '{letter["character"]}' character was found {letter["number"]} times.")
+    print()
+    print("--- End report ---")
 
 def get_book_test(path):
     with open(path) as f:
@@ -24,5 +32,14 @@ def letter_count(words):
             pass
     return letter_dict
 
+def sort_on(dict):
+    return dict["number"]
+
+def organize(dict):
+    alpha_list = []
+    for letter in dict:
+        alpha_list.append({"character": letter, "number": dict[letter]})
+    alpha_list.sort(reverse=True, key=sort_on)
+    return alpha_list
 
 main()
