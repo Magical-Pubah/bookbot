21b5ef85c624a95ec5492085dd330369fdcaa378
Magical-Pubah <bconnellypitts@gmail.com>
2024-04-25 23:04:08 -0700

    Added word count function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     story = get_book_test(book_path)
-    print(story)
+    print(word_count(story))
 
 def get_book_test(path):
     with open(path) as f:
         return f.read()
+
+def word_count(story):
+    words = story.split()
+    return len(words)
 
 
 
