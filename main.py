3727ce6d324a64a3f37aedb66bd1d447dc1240b4
Magical-Pubah <bconnellypitts@gmail.com>
2024-04-26 00:15:17 -0700

    Added letter count, optmization needed.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     book_path = "books/frankenstein.txt"
     story = get_book_test(book_path)
-    print(word_count(story))
+    print(f"There are {word_count(story)} words in this book.")
+    print(letter_count(story))
 
 def get_book_test(path):
     with open(path) as f:
@@ -11,6 +12,17 @@ def word_count(story):
     words = story.split()
     return len(words)
 
+def letter_count(words):
+    letters = list(words.lower())
+    letter_dict = {}
+    for letter in letters:
+        if letter in letter_dict:
+            pass
+        elif letter.isalpha():
+            letter_dict.setdefault(letter, letters.count(letter))
+        else:
+            pass
+    return letter_dict
 
 
 main()
